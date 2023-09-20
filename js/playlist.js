50816fa8d442e4894737a3c86b1a29e003128586
Salim Anwar <119126143+saalimanwar@users.noreply.github.com>
2023-09-20 16:26:57 +0530

    Add files via upload
    music player

---
+++ js/playlist.js
@@ -0,0 +1,52 @@
+const songs = [
+  {
+    name: " Mere Humsafar ",
+    artist: "Amanat Ali, Zaheer Abbas",
+    img: "img1",
+    audio: "music1"
+  },
+
+  {
+    name: "Parizaad ",
+    artist: "Asrar, Waqar Ali",
+    img: "img2",
+    audio: "music2"
+  },
+
+  {
+    name: "O Rungreza",
+    artist: "Sahir Ali Bagga",
+    img: "img3",
+    audio: "music3"
+  },
+
+  {
+    name: "Dil Sambhal Ja Zara ",
+    artist: "Mohammed Irfan ",
+    img: "img4",
+    audio: "music4"
+  },
+
+  {
+    name: " Khaani ",
+    artist: "Rahat Fateh Ali Khan",
+    img: "img5",
+    audio: "music5"
+  },
+
+  {
+    name: " Laal Ishq ",
+    artist: "Rahat Fateh Ali Khan",
+    img: "img6",
+    audio: "music6"
+  },
+
+  {
+    name: " Mera Yaar ",
+    artist: "Sahir Ali Bagga",
+    img: "img7",
+    audio: "music7"
+  },
+  
+
+]
